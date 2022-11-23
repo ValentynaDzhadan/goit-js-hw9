64313d2642bf9bbaf87172d60d9c49d7f2f78617
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2022-11-23 19:33:09 +0200

    added seconds task

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let currentData;
+let selectedData;
+
+buttonStartEl.setAttribute('disabled', true);
+buttonStopEl.setAttribute('disabled', true);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedData = selectedDates[0].getTime(); // time in ms
+    currentData = options.defaultDate.getTime(); // time in ms
+    if (selectedData < currentData) {
+      Notiflix.Notify.warning('Please choose a date in the future'); //  HELP it's difficult to see the text(
+      buttonStartEl.setAttribute('disabled', true);
+    } else {
+      buttonStartEl.removeAttribute('disabled');
+      daysEl.textContent = '00';
+      hoursEl.textContent = '00';
+      minutesEl.textContent = '00';
+      secondsEl.textContent = '00';
+    }
+  },
+};
+flatpickr(inputEl, options);
+
+buttonStartEl.addEventListener('click', startTimer);
+buttonStopEl.addEventListener('click', stopTimer);
+
+function stopTimer(event) {
+  event.target.setAttribute('disabled', true);
+  inputEl.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+}
+
+let intervalId = null;
+
+function startTimer(event) {
+  event.target.setAttribute('disabled', true);
+  inputEl.setAttribute('disabled', true);
+  buttonStopEl.removeAttribute('disabled');
+
+  let deltaDate = selectedData - currentData;
+
+  intervalId = setInterval(() => {
+    if (deltaDate > 999) {
+      // HELP which number here 999 or 1000, cant understand the logic
+      deltaDate -= 1000;
+      const { days, hours, minutes, seconds } = convertMs(deltaDate);
+      daysEl.textContent = addLeadingZero(days);
+      hoursEl.textContent = addLeadingZero(hours);
+      minutesEl.textContent = addLeadingZero(minutes);
+      secondsEl.textContent = addLeadingZero(seconds);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
